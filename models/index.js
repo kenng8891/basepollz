0a846e597a9727d030e05b8206f8a351116b56ea
Kenng8891 <ng.kennyy@gmail.com>
2022-07-26 17:00:01 -0700

    eliminate index.js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,10 @@
 // import all models
-const Game = require('./Game');
-const User = require('./User');
-const Vote = require('./Vote');
-const Comment = require('./Comment');
+const Game = require("./Game");
+const User = require("./User");
+const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 // create associations
-
-//do not need:
-// User.hasMany(Game, {
-//   foreignKey: 'user_id'
-// });
-// Game.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
-
 
 //ADD ALL THESE BACK IN WHEN WE ADD VOTING
 
@@ -51,117 +41,20 @@ const Comment = require('./Comment');
 
 // Users and comments
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 //games and comments
 Game.hasMany(Comment, {
-  foreignKey: 'game_id'
+  foreignKey: "game_id",
 });
 
 Comment.belongsTo(Game, {
-  foreignKey: 'game_id',
+  foreignKey: "game_id",
 });
 
-
-// // //NEW: make sure this is correct
-// Game.belongsToMany(Team, {
-//   through: GameTeam,
-//   as: "team_id",
-//   foreignKey: 'game_id',
-  
-// });
-
-// Team.belongsToMany(Game, {
-//   through: GameTeam,
-//   as: "game_id",
-//   foreignKey: 'team_id',
- 
-// });
-
-
-module.exports = { User, Game, Vote, Comment};
-
-
-
-
-
-// // import all models
-// const Game = require("./Game");
-// const Team = require("./Team");
-// const User = require("./User");
-// const Vote = require("./Vote");
-// const Comment = require("./Comment");
-// const GameTeam = require("./GameTeam")
-
-// // create associations
-// User.hasMany(Game, {
-//   foreignKey: "user_id",
-// });
-
-// Game.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// User.belongsToMany(Game, {
-//   through: Vote,
-//   as: "voted_games",
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Game.belongsToMany(User, {
-//   through: Vote,
-//   as: "voted_games",
-//   foreignKey: "game_id",
-//   onDelete: "SET NULL",
-// });
-
-// Vote.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Vote.belongsTo(Game, {
-//   foreignKey: "game_id",
-//   onDelete: "SET NULL",
-// });
-
-// User.hasMany(Vote, {
-//   foreignKey: "user_id",
-// });
-
-// Game.hasMany(Vote, {
-//   foreignKey: "game_id",
-// });
-
-// Comment.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Comment.belongsTo(Game, {
-//   foreignKey: "game_id",
-//   onDelete: "SET NULL",
-// });
-
-// User.hasMany(Comment, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Game.hasMany(Comment, {
-//   foreignKey: "game_id",
-// });
-
-// // //NEW: make sure this is correct
-// Game.hasMany(Team, {
-//   foreignKey: "team_id",
-// });
-
-// module.exports = { User, Game, Vote, Comment, Team, GameTeam };
+module.exports = { User, Game, Vote, Comment };
